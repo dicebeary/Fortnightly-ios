9a6902785ab951fc134aecafb8c58cb46f220aac
Vajda Kristóf <vajda.kristof@hiflylabs.hu>
2021-07-16 15:54:23 +0200

    [FN-1] Create plain project

---
+++ Fortnightly/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Fortnightly
+//
+//  Created by Vajda Kristóf on 2021. 07. 16..
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
